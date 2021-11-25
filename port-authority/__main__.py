ee0b2bd5549942edb5c50b2b5017ab0faaf37cf0
bluedog8050 <bluedog8050@hotmail.com>
2021-11-24 16:19:18 -0800

    initial commit

---
+++ port-authority/__main__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Main Module
+"""
+
+__author__ = "Bluedog8050"
+__version__ = "0.1.0"
+__license__ = "GNU 3.0"
+
+
+def main():
+    """ Main entry point of the app """
+    print("hello world")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
